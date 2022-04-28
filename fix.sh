f4dc9ee9e1a02dccde447b8faa88442528a71813
marzeq <58303665+marzeq@users.noreply.github.com>
2022-04-28 15:31:43 +0200

    fix for current session

--- fix.sh
+++ fix.sh
@@ -1,4 +1,9 @@
+# For current session
+echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
+
+# For after reboot
 echo options hid_apple fnmode=0 | sudo tee -a /etc/modprobe.d/hid_apple.conf
+
 if command -v mkinitcpio &> /dev/null
 then
   sudo mkinitcpio -P
@@ -6,5 +11,5 @@ elif command -v update-initramfs &> /dev/null
 then
   sudo update-initramfs
 else
-  echo "Could not find mkinitcpio or update-initramfs. Sorry!"
+  echo "Could not find mkinitcpio or update-initramfs. Please a find an alternative for your distribution."
 fi
